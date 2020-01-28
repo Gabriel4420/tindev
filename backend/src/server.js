308bd074e8e435e3078ebf240fa6339c20be758d
Gabriel Rodrigues <gabriel_rodrigues_perez@hotmail.com>
2020-01-28 13:43:28 -0300

    novas alterações

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,14 @@
 const express = require('express');
+// neste momento ja se tem um servidor, porem ele está mudo, não ouve 
+// nenhum endereço.
+const server = express();
+//operações de request e response 
+server.get('/',(req,res) => {
+    var name = req.query.name;
+
+    res.json({message:`Hello ${name}`});
+});
 
 
+//serve para ouvir uma porta no servidor por assim dizer
+server.listen(3333);
