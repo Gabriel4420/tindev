e099393e8df2a54b83161a0653acccafcb9e2154
Gabriel Rodrigues <gabriel_rodrigues_perez@hotmail.com>
2020-01-29 03:34:31 -0300

    importando cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 // neste momento ja se tem um servidor, porem ele está mudo, não ouve 
 // nenhum endereço.
 const server = express();
 
+//metodo para conectar ao banco de dados 
 mongoose.connect('mongodb+srv://tindev:AgCTDTp3VoDySgGP@cluster0-lpzif.mongodb.net/test?retryWrites=true&w=majority', {
     useUnifiedTopology: true,
     useNewUrlParser:true
 });
-
+server.use(cors());
 server.use(express.json());
 // serve para quando queremos colocar algum tipo de configuração 
 // que está em outro arquivo.
