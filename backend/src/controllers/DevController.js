91aa10924604ba4dad1b4e85e38d7c78a227ec78
Gabriel Rodrigues <gabriel_rodrigues_perez@hotmail.com>
2020-01-29 00:09:11 -0300

    criação de controllers e alterações de arquivos

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,9 @@
+const axios = require('axios');
 module.exports = {
     async store(req,res){
+        console.log(req.body.username);
+
+        const { username } =  req.body;
         return res.json({ok:true});
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,23 @@
 const express = require('express');
-
+const devController = require('./controllers/DevController');
 const routes = express.Router();
 
 
+
 //operações de request e response 
-routes.get('/',(req,res) => {
+/* routes.get('/',(req,res) => {
     var name = req.query.name;
 
     res.json({message:`Hello ${name}`});
-});
+}); */
 
-//operações de cadastro
+//operações de cadastro sem controller
 
-routes.post('/devs', (req,res) => {
+/* routes.post('/devs', (req,res) => {
     return res.json(req.body);
-});
+}); */
+
+//operações de cadastro com controller
+routes.post('/devs',devController.store);
 
 module.exports = routes;
