02591a6fc2b935697c8ea9c235ff7b8cc67799e2
Gabriel Rodrigues <gabriel_rodrigues_perez@hotmail.com>
2020-01-29 00:17:07 -0300

    alterações em DevController

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 module.exports = {
     async store(req,res){
-        console.log(req.body.username);
-
+        
         const { username } =  req.body;
+        
+        const response = axios.get(`https://api.github.com/users/${ username }`);
         return res.json({ok:true});
     }
 };
